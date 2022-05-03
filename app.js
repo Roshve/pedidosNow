2180cccccd608ad18a9620b1b9c17658fd1ff72e
Luis Cova <luis.cova@bigbox.com.ar>
2022-05-02 22:54:18 -0300

    feat: new logic

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ Vue.component("challenge-table", {
   template: `
   <section>
     <table class="table" border="0" cellspacing="0" cellpadding="0">
-      <tr class="table-header">
-        <th>ID</th>
-        <th>Nombre</th>
-        <th>Apellido</th>
-        <th>Provincia</th>
+      <tr v-if="header" class="table-header">
+        <th v-for="(title, index) in titles" :keys="index">{{ title }}</th>
       <tr/>
       <tr :class="tablePar(index)" v-for="(user, index) in users" :key="index">
-        <th>{{ index + 1 }}</th>
+        <th>{{ user.id}}</th>
         <th>{{ user.name }}</th>
         <th>{{ user.lastName }}</th>
         <th>{{ user.province }}</th>
@@ -17,6 +14,16 @@ Vue.component("challenge-table", {
     </table>
   </section>`,
   props: {
+    header: {
+      type: Boolean,
+      default: false,
+      require: false,
+    },
+    titles: {
+      type: Array,
+      default: () => [],
+      require: false
+    },
     users: {
       type: Array,
       default: () => [],
@@ -34,37 +41,99 @@ var app = new Vue({
   el: '#app',
 
   data: {
-    users: [
-      {
-        id: 1,
-        name: 'Jose',
-        lastName: 'Velez',
-        province: 'San Luis'
-      },
-      {
-        id: 2,
-        name: 'Ramon',
-        lastName: 'Gomez',
-        province: 'Santa Fe'
-      },
-      {
-        id: 3,
-        name: 'Marcelo',
-        lastName: 'Godino',
-        province: 'Buenos Aires'
-      },
-      {
-        id: 4,
-        name: 'Ramiro',
-        lastName: 'Gimenez',
-        province: 'Salta'
-      }
-    ]
+    first: {
+      titles: ['ID', 'Nombre', 'Apellido', 'Provincia'],
+      users: [
+        {
+          id: 1,
+          name: 'Jose',
+          lastName: 'Velez',
+          province: 'San Luis'
+        },
+        {
+          id: 2,
+          name: 'Ramon',
+          lastName: 'Gomez',
+          province: 'Santa Fe'
+        },
+        {
+          id: 3,
+          name: 'Marcelo',
+          lastName: 'Godino',
+          province: 'Buenos Aires'
+        },
+        {
+          id: 4,
+          name: 'Ramiro',
+          lastName: 'Gimenez',
+          province: 'Salta'
+        }
+      ],
+    },
+    second: {
+      titles: ['ID', 'Nombre', 'Apellido', 'Provincia'],
+      users: [
+        {
+          id: 1,
+          name: 'Jose',
+          lastName: 'Velez',
+          province: 'San Luis'
+        },
+        {
+          id: 2,
+          name: 'Ramon',
+          lastName: 'Gomez',
+          province: 'Santa Fe'
+        },
+        {
+          id: 3,
+          name: 'Marcelo',
+          lastName: 'Godino',
+          province: 'Buenos Aires'
+        },
+        {
+          id: 4,
+          name: 'Ramiro',
+          lastName: 'Gimenez',
+          province: 'Salta'
+        }
+      ],
+    },
+    third: {
+      titles: ['ID', 'Nombre', 'Apellido', 'Provincia'],
+      users: [
+        {
+          id: 1,
+          name: 'Jose',
+          lastName: 'Velez',
+          province: 'San Luis'
+        },
+        {
+          id: 2,
+          name: 'Ramon',
+          lastName: 'Gomez',
+          province: 'Santa Fe'
+        },
+        {
+          id: 3,
+          name: 'Marcelo',
+          lastName: 'Godino',
+          province: 'Buenos Aires'
+        },
+        {
+          id: 4,
+          name: 'Ramiro',
+          lastName: 'Gimenez',
+          province: 'Salta'
+        }
+      ],
+    },
   },
 
   template: `
   <main class="box">
-    <challenge-table :users="users">
-    </challenge-table>
+    <challenge-table :users="first.users" :titles="first.titles" header />
+    <challenge-table :users="second.users" />
+    <challenge-table :users="third.users" />
   </main>`,
 })
