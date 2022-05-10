3a4de503a54643087eb84df96ac1d9930b4f3e3a
Luis Cova <luis.cova@bigbox.com.ar>
2022-05-09 21:21:38 -0300

    feat: add challenge and fix lints

---
+++ src/App.vue
@@ -0,0 +1,108 @@
+<template>
+  <main id="app" class="box">
+    <challenge-table :users="first.users" :titles="first.titles" header />
+    <challenge-table :users="second.users" />
+    <challenge-table :users="third.users" />
+  </main>
+</template>
+
+<script>
+import ChallengeTable from "./components/ChallengeTable.vue";
+
+export default {
+  name: "App",
+  components: {
+    ChallengeTable
+  },
+
+  data: () => ({
+    first: {
+      titles: ["ID", "Nombre", "Apellido", "Provincia"],
+      users: [
+        {
+          id: 1,
+          name: "Jose",
+          lastName: "Velez",
+          province: "San Luis",
+        },
+        {
+          id: 2,
+          name: "Ramon",
+          lastName: "Gomez",
+          province: "Santa Fe",
+        },
+        {
+          id: 3,
+          name: "Marcelo",
+          lastName: "Godino",
+          province: "Buenos Aires",
+        },
+        {
+          id: 4,
+          name: "Ramiro",
+          lastName: "Gimenez",
+          province: "Salta",
+        },
+      ],
+    },
+    second: {
+      titles: ["ID", "Nombre", "Apellido", "Provincia"],
+      users: [
+        {
+          id: 1,
+          name: "Jose",
+          lastName: "Velez",
+          province: "San Luis",
+        },
+        {
+          id: 2,
+          name: "Ramon",
+          lastName: "Gomez",
+          province: "Santa Fe",
+        },
+        {
+          id: 3,
+          name: "Marcelo",
+          lastName: "Godino",
+          province: "Buenos Aires",
+        },
+        {
+          id: 4,
+          name: "Ramiro",
+          lastName: "Gimenez",
+          province: "Salta",
+        },
+      ],
+    },
+    third: {
+      titles: ["ID", "Nombre", "Apellido", "Provincia"],
+      users: [
+        {
+          id: 1,
+          name: "Jose",
+          lastName: "Velez",
+          province: "San Luis",
+        },
+        {
+          id: 2,
+          name: "Ramon",
+          lastName: "Gomez",
+          province: "Santa Fe",
+        },
+        {
+          id: 3,
+          name: "Marcelo",
+          lastName: "Godino",
+          province: "Buenos Aires",
+        },
+        {
+          id: 4,
+          name: "Ramiro",
+          lastName: "Gimenez",
+          province: "Salta",
+        },
+      ],
+    },
+  }),
+};
+</script>
---
+++ src/components/ChallengeTable.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <table class="table" border="0" cellspacing="0" cellpadding="0">
+      <tr v-if="header" class="table-header">
+        <th v-for="(title, index) in titles" :key="index">{{ title }}</th>
+      </tr>
+      <tr />
+      <tr :class="tablePar(index)" v-for="(user, index) in users" :key="index">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+        <td>{{ user.lastName }}</td>
+        <td>{{ user.province }}</td>
+      </tr>
+
+      <tr />
+    </table>
+  </section>
+</template>
+<script>
+export default {
+  name: 'ChallengeTable',
+
+  props: {
+    header: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+    titles: {
+      type: Array,
+      default: () => [],
+      required: false,
+    },
+    users: {
+      type: Array,
+      default: () => [],
+      required: true,
+    },
+  },
+
+  methods: {
+    tablePar(index) {
+      return index % 2 === 0 ? "table-content-par" : "table-content-impar";
+    },
+  },
+};
+</script>
