36b8cfdf58f462ad9dbf01bb125f8c8b63f951ce
Luis Cova <luis.cova@bigbox.com.ar>
2022-05-11 22:52:48 -0300

    feat: cards

--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -1,17 +1,32 @@
 <template>
   <main>
     <base-header />
+    <div class="container">
+      <div class="row">
+        <base-card class="col-4"/>
+        <base-card class="col-4" />
+        <base-card class="col-4" />
+        <base-card class="col-4" />
+        <base-card class="col-4" />
+        <base-card class="col-4" />
+        <base-card class="col-4" />
+        <base-card class="col-4"/>
+        <base-card class="col-4"/>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
 import BaseHeader from "@/components/BaseHeader.vue";
+import BaseCard from "@/components/BaseCard.vue";
 
 export default {
   name: "ViewHome",
 
   components: {
     BaseHeader,
+    BaseCard,
   },
 };
 </script>
